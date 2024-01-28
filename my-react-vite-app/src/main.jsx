f0529000dce05c196bf1c15014df9efc6139a3bd
EAN-GIT <emmanueljoeemie@gmail.com>
2024-01-28 22:42:08 +0100

    first commit

---
+++ my-react-vite-app/src/main.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./components/app";
+
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(<App />, document.getElementById("root"));

18d6fe4aad6585e41967c877ec06952bc21b9afe
EAN-GIT <emmanueljoeemie@gmail.com>
2024-02-26 18:10:48 +0100

    add moie search functionality

---
+++ my-react-vite-app/src/components/Search/Search.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Search as SearchIcon } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+
+import useStyles from "./styles";
+import { InputAdornment, TextField } from "@mui/material";
+import { searchMovie } from "../../features/currentGenreOrCategory";
+
+const Search = () => {
+  const classes = useStyles();
+  const [query, setQuery] = useState("");
+  const dispatch = useDispatch();
+
+  // dispatch action to get the coreesponding movies
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      dispatch(searchMovie(query));
+    //   setQuery('')
+    }
+  };
+  return (
+    <div className={classes.searchContainer}>
+      <TextField
+        onKeyDown={handleKeyPress}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        variant="standard"
+        // placeholder etxt
+        InputProps={{
+          className: classes.input,
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </div>
+  );
+};
+
+export default Search;
--- my-react-vite-app/src/components/index.js
+++ my-react-vite-app/src/components/index.js
@@ -3,7 +3,9 @@ export { default as Movieinfo } from "./Movieinfo/Movieinfo";
 export { default as Movies } from "./Movies/Movies";
 export { default as Profile } from "./Profile/Profile";
 export { default as NavBar } from "./NavBar/NavBar";
-// export { default as SideBar } from "./Sidebar/Sidebar";
+export { default as Sidebar } from "./Sidebar/Sidebar";
 
 export { default as MovieList } from "./MovieList/MovieList";
 export { default as Movie } from "./Movie/Movie";
+export { default as Search } from "./Search/Search";
+

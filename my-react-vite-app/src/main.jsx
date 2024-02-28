3446bb3ba62939eda47547dac6b0f4cf675a3504
EAN-GIT <emmanueljoeemie@gmail.com>
2024-02-28 13:49:16 -0800

    add profile page

--- my-react-vite-app/src/main.jsx
+++ my-react-vite-app/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import React from "react"; 
-import App from "./components/app";
+import App from "./components/app.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 

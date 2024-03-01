2496862f641e7c80d1c82c61cdcafa4938c4cda5
EAN-GIT <emmanueljoeemie@gmail.com>
2024-02-29 21:15:01 -0800

    add movie recommendation feature

--- my-react-vite-app/src/services/TMDB.js
+++ my-react-vite-app/src/services/TMDB.js
@@ -52,10 +52,14 @@ export const tmdbApi = createApi({
     getMovie: builder.query({
       query: (id) =>
         `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`
-    })
+    }),
+//* Get user specific lists
+getRecommendations: builder.query({
+query:({movie_id, list})=>`/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`
+})
   })
 });
 
 //redux automatically creates hooks for each query
-export const { useGetMoviesQuery, useGetGenreQuery, useGetMovieQuery } =
+export const { useGetMoviesQuery, useGetGenreQuery, useGetMovieQuery, useGetRecommendationsQuery } =
   tmdbApi;

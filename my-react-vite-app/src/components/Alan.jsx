5e76a4231ee4032ff1664e341a4aac9dbe82a51c
EAN-GIT <emmanueljoeemie@gmail.com>
2024-03-03 23:56:57 -0800

    add alan ai functionality for login n searchmovies

--- my-react-vite-app/src/components/Alan.jsx
+++ my-react-vite-app/src/components/Alan.jsx
@@ -3,24 +3,49 @@ import React, { useContext, useEffect } from 'react';
 
 import { ColorModeContext } from '../utils/ToggleColorMode';
 
-// alan is not a component its a custom hook...so its doesnt return ny thing
 
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
+import { fetchToken } from '../utils';
+
+// alan is not a component its a custom hook...so its doesnt return ny thing
 const useAlan = () => {
-  const { setMode } = useContext(ColorModeContext);
+    const { setMode } = useContext(ColorModeContext);
+    const dispatch = useDispatch();
+    const history = useHistory();
 
   useEffect(() => {
     alanBtn({
       key: 'b56e87b57b0cd7ee4e9f48947d327f0e2e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: ({ command, mode }) => {
-        if (command === 'changeMode') {
-          // Call the client code that will react to the received command
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === 'chooseGenre') {
+          const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+
+          if (foundGenre) {
+            history.push('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+            history.push('/');
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === 'changeMode') {
           if (mode === 'light') {
             setMode('light');
           } else {
             setMode('dark');
           }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          localStorage.clear();
+          history.push('/');
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
         }
-      }
+      },
     });
   }, []);
 };
